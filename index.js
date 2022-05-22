e4237dbcb4e035e2938d25c9ea46ea2724432e73
Juliette <81535733+JulietteL14307@users.noreply.github.com>
2022-05-22 14:13:45 -0500

    finished first demo of about/landing page

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// flower nav animation
+
 $(window).load(function(){
     $('.toggle').addClass('menu');
     $('.toggle').click(function(){
@@ -6,3 +8,21 @@ $(window).load(function(){
         $(".menu").toggleClass("open");
     });
 });
+
+// read more
+
+function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("moreBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  };
